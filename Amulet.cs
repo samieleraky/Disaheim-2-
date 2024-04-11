de969b4cefd642c0b38905602228323a127b8519
samieleraky <157993269+samieleraky@users.noreply.github.com>
2024-04-11 07:43:04 +0200

    Add files via upload

---
+++ Amulet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace disaheim
+{
+    public class Amulet
+    {
+        // Private fields
+        public string _itemId;
+        public string _design;
+        public Level _quality; // Corrected the declaration of _quality
+
+        // Constructors
+        public Amulet(string itemId)
+        {
+            _itemId = itemId;
+        }
+
+        public Amulet(string itemId, Level quality)
+        {
+            _itemId = itemId;
+            _quality = quality;
+        }
+
+        public Amulet(string itemId, Level quality, string design)
+        {
+            _itemId = itemId;
+            _quality = quality;
+            _design = design;
+        }
+
+        // Properties
+        public string ItemId
+        {
+            get { return _itemId; }
+            set { _itemId = value; }
+        }
+
+        public string Design
+        {
+            get { return _design; }
+            set { _design = value; }
+        }
+
+        public Level Quality
+        {
+            get { return _quality; }
+            set { _quality = value; }
+        }
+
+        // ToString method override
+        public override string ToString()
+        {
+            return $"Amulet: ItemId - {_itemId}, Design - {_design}, Quality - {_quality}";
+        }
+    }
+}
---
+++ Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace disaheim
+{
+    public class Book
+    {
+
+        public string _ItemId;
+        public string _Title;
+        public double _price;
+
+        //Constructors
+        public Book(string itemId)
+        {
+            _ItemId = itemId;
+        }
+
+        public Book(string itemId, string title)
+        {
+            _ItemId = itemId;
+            _Title = title;
+        }
+
+        public Book(string itemId, string title, double price)
+        {
+            _ItemId = itemId;
+            _Title = title;
+            _price = price;
+
+        }
+
+        //properties
+        public string Itemid
+        { get { return _ItemId; } }
+
+        public string Title
+        { get { return _Title; } }
+
+        public double Price
+        { get { return _price; } }
+
+        public override string ToString()
+        {
+            return $"Book: ItemId - {_ItemId}, Title - {_Title}, Price - {_price}";
+        }
+    }
+
+}
